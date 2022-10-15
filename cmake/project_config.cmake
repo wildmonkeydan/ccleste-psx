9378d0bee2f591c9da54f87c19238064532d1ec2
Michael Fitzmayer <mail@michael-fitzmayer.de>
2022-10-15 14:53:24 +0200

    Minor re-organisation

--- cmake/project_config.cmake
+++ cmake/project_config.cmake
@@ -11,8 +11,10 @@ set(APP_UID  0x10005731) # launcher.app UID
 set(APP_NAME "Celeste")
 
 file(MAKE_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/export/data)
-configure_file(${CMAKE_CURRENT_SOURCE_DIR}/data/font.bmp ${CMAKE_CURRENT_SOURCE_DIR}/export/data COPYONLY)
-configure_file(${CMAKE_CURRENT_SOURCE_DIR}/data/gfx.bmp  ${CMAKE_CURRENT_SOURCE_DIR}/export/data COPYONLY)
+configure_file(${CMAKE_CURRENT_SOURCE_DIR}/data/font.bmp             ${CMAKE_CURRENT_SOURCE_DIR}/export/data COPYONLY)
+configure_file(${CMAKE_CURRENT_SOURCE_DIR}/data/gfx.bmp              ${CMAKE_CURRENT_SOURCE_DIR}/export/data COPYONLY)
+configure_file(${CMAKE_CURRENT_SOURCE_DIR}/data/test-tas.txt         ${CMAKE_CURRENT_SOURCE_DIR}/export      COPYONLY)
+configure_file(${CMAKE_CURRENT_SOURCE_DIR}/data/gamecontrollerdb.txt ${CMAKE_CURRENT_SOURCE_DIR}/export      COPYONLY)
 
 if(CELESTE_P8_FIXEDP)
   SET_SOURCE_FILES_PROPERTIES(${CMAKE_CURRENT_SOURCE_DIR}/src/celeste.c PROPERTIES LANGUAGE CXX )
